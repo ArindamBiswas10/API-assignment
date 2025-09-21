8eeb3eb124196242af01ed03cb5d0ab8e9559bef
ArindamBiswas10 <arindombiswas29@gmail.com>
2025-09-22 04:15:19 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -3,20 +3,26 @@ const authRoutes = require("./routes/authRoutes");
 const { errorHandler } = require("./middlewares/errorHandler");
 const connectDB = require("./config/db");
 const app = express();
+app.use(errorHandler);
+
+
 require('dotenv').config();
 
 app.use(express.json());
+/*const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:3001', 
+  credentials: true,               
+}));*/
 
 app.use("/api/auth",authRoutes);
-app.use(errorHandler);
+
 
 async function startServer() {
   try {
     await connectDB();
     console.log("MongoDB connected");
-    console.log(process.env.SENDGRID_API_KEY, process.env.EMAIL_PASS);
-
-
     app.listen(3000, () => {
       console.log("Server running on port 3000");
     });
